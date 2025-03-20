ae328da25d811a9282b510f846b0981f853103fe
rising3 <michio.nakagawa@gmail.com>
2025-03-21 03:56:15 +0900

    feat: generic methods

--- README.md
+++ README.md
@@ -1,2 +1,263 @@
 # smart-entity-js
-A smart entity that implements general-purpose methods such as clone(), toJSON(), and fromJSON().
+
+A smart entity that implements generic methods such as clone(), toJSON(), and fromJSON().
+
+## Install
+
+To install:
+```
+npm i
+```
+
+## Getting started
+
+To run the example:
+```
+npm start
+```
+### Creating an Entity
+
+Create an entity by inheriting the BaseEntity class.
+As an example, create a Person class and an Address class.
+
+#### Person Class
+``` typescript
+class Person extends BaseEntity<Person> {
+    protected _maskableFields = ["name"];
+    protected _requiredFields = ["name"];
+    protected _schemaHints = {
+        id: { type: "string" },
+        name: { type: "string" },
+        age: { type: "number", nullable: true },
+        isActive: { type: "boolean", nullable: false },
+        createAt: { type: "number" },
+        hobbies: { type: "array", nullable: true },
+        address: { type: "object", schema: Address.getJsonSchema(), nullable: true },
+    };
+
+    constructor(
+        public id: string = crypto.randomUUID(),
+        public name: string = "",
+        public age?: number,
+        public isActive: boolean = true,
+        public createAt: number = Date.now(),
+        public hobbies: string[] = [],
+        public address?: Address
+    ) {
+        super();
+    }
+
+    static example(): Person {
+        return new Person(
+            crypto.randomUUID(),
+            "John Doe",
+            Math.floor(Math.random() * 100) + 1,
+            true,
+            Date.now(),
+            ["reading", "video game"],
+            Address.example()
+        );
+    }
+}
+```
+#### Address Class
+
+``` typescript
+class Address extends BaseEntity<Address> {
+    protected _maskableFields = ["postalCode", "address"];
+    protected _requiredFields = ["postalCode", "address"];
+    protected _schemaHints = {
+        postalCode: { type: "string" },
+        address: { type: "string" },
+    };
+
+    constructor(
+        public postalCode: string = "",
+        public address: string = ""
+    ) {
+        super();
+    }
+
+    static example(): Address {
+        return new Address(
+            "123-4567",
+            "tokyo"
+        );
+    }
+}
+```
+
+### Generic methods
+
+#### static getJsonSchema()
+
+Get the JSON schema for the entity.
+The JSON schema is created based on the hints defined in the entity.
+
+``` typescript
+const schema = Person.getJsonSchema();
+```
+
+result:
+``` json
+{
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "string",
+      "nullable": false
+    },
+    "name": {
+      "type": "string",
+      "nullable": false
+    },
+    "age": {
+      "type": "number",
+      "nullable": true
+    },
+    "isActive": {
+      "type": "boolean",
+      "nullable": false
+    },
+    "createAt": {
+      "type": "number",
+      "nullable": false
+    },
+    "hobbies": {
+      "type": "array",
+      "nullable": true
+    },
+    "address": {
+      "type": "object",
+      "properties": {
+        "postalCode": {
+          "type": "string",
+          "nullable": false
+        },
+        "address": {
+          "type": "string",
+          "nullable": false
+        }
+      },
+      "required": [
+        "postalCode",
+        "address"
+      ],
+      "additionalProperties": false,
+      "nullable": true
+    }
+  },
+  "required": [
+    "name"
+  ],
+  "additionalProperties": false
+}
+```
+
+#### static fromJSON()
+
+Create an instance of the Person class from JSON. 
+Validate the JSON using a JSON Schema.
+
+``` typescript
+const json = JSON.stringify({
+  id: "b46021c4-2cf2-4167-a31c-50c9d297e6b8",
+  name: "Jhon Doe",
+  age: 1,
+  isActive: false,
+  createAt: 1742483906966,
+  hobbies: ["Reading", "video game"],
+  address: { postalCode: "123-4567", address: "tokyo" },
+});
+
+const person = Person.from(json);
+```
+
+#### static example()
+
+Create an instance of the example Person class.
+
+``` typescript
+const person = Person.example();
+```
+
+#### toJSON()
+
+Get JSON from an instance of the Person class.
+
+##### compact JSON
+
+``` typescript
+person.toJSON();
+```
+
+result:
+``` json
+{"id":"e110860c-c201-4977-8f5f-97c7eb0e31ba","name":"John Doe","age":5,"isActive":true,"createAt":1742492794876,"hobbies":["reading","video game"],"address":{"postalCode":"123-4567","address":"tokyo"}}
+```
+
+##### pretty JSON
+
+``` typescript
+person.toJSON(true);
+```
+
+result:
+``` json
+{
+  "id": "e110860c-c201-4977-8f5f-97c7eb0e31ba",
+  "name": "John Doe",
+  "age": 5,
+  "isActive": true,
+  "createAt": 1742492794876,
+  "hobbies": [
+    "reading",
+    "video game"
+  ],
+  "address": {
+    "postalCode": "123-4567",
+    "address": "tokyo"
+  }
+}
+```
+##### masked JSON
+
+``` typescript
+person.toJSON(true, true);
+```
+
+result:
+``` json
+{
+  "id": "e110860c-c201-4977-8f5f-97c7eb0e31ba",
+  "name": "********",
+  "age": 5,
+  "isActive": true,
+  "createAt": 1742492794876,
+  "hobbies": [
+    "reading",
+    "video game"
+  ],
+  "address": {
+    "postalCode": "********",
+    "address": "*****"
+  }
+}
+```
+
+#### validate()
+
+Validate an instance of the Person class using a JSON Schema.
+
+``` typescript
+const person = new Person();
+person.validate();  // Throw Error
+```
+
+#### clone()
+
+Create a clone by deep copying an instance of the Person class.
+
+``` typescript
+const clone = person.clone();
+```
---
+++ __tests__/Person.test.ts
@@ -0,0 +1,196 @@
+import { describe, expect, test } from "@jest/globals";
+import Person from "../src/Person";
+import Address from "../src/Address";
+
+describe("Person Class", () => {
+  test("toJSON() should return valid JSON", () => {
+    const person = new Person(
+      "4c581c64-94fc-4880-b6e1-6130fbdc7fab",
+      "John Doe",
+      30,
+      true,
+      1,
+      ["reading", "video game"],
+      new Address("123-4567", "tokyo")
+    );
+    const json = person.toJSON();
+
+    expect(JSON.parse(json)).toEqual({
+      id: "4c581c64-94fc-4880-b6e1-6130fbdc7fab",
+      name: "John Doe",
+      age: 30,
+      isActive: true,
+      createAt: 1,
+      hobbies: ["reading", "video game"],
+      address: { postalCode: "123-4567", address: "tokyo" },
+    });
+  });
+
+  test("toJSON() should return pretty formatted JSON when pretty=true", () => {
+    const person = new Person(
+      "4c581c64-94fc-4880-b6e1-6130fbdc7fab",
+      "John Doe",
+      30,
+      true,
+      1,
+      ["reading", "video game"],
+      new Address("123-4567", "tokyo")
+    );
+    const prettyJson = person.toJSON(true);
+
+    expect(prettyJson).toContain("\n");
+    expect(prettyJson).toContain("  ");
+    expect(JSON.parse(prettyJson)).toEqual({
+      id: "4c581c64-94fc-4880-b6e1-6130fbdc7fab",
+      name: "John Doe",
+      age: 30,
+      isActive: true,
+      createAt: 1,
+      hobbies: ["reading", "video game"],
+      address: { postalCode: "123-4567", address: "tokyo" },
+    });
+  });
+
+  test("toJSON() should return compact JSON when pretty=false", () => {
+    const person = new Person(
+      "4c581c64-94fc-4880-b6e1-6130fbdc7fab",
+      "John Doe",
+      30,
+      true,
+      1,
+      ["reading", "video game"],
+      new Address("123-4567", "tokyo")
+    );
+    const compactJson = person.toJSON(false);
+
+    expect(compactJson).not.toContain("\n");
+    expect(compactJson).not.toContain("  ");
+    expect(JSON.parse(compactJson)).toEqual({
+      id: "4c581c64-94fc-4880-b6e1-6130fbdc7fab",
+      name: "John Doe",
+      age: 30,
+      isActive: true,
+      createAt: 1,
+      hobbies: ["reading", "video game"],
+      address: { postalCode: "123-4567", address: "tokyo" },
+    });
+  });
+
+  test("toJSON() should mask sensitive fields", () => {
+    const person = new Person(
+      "4c581c64-94fc-4880-b6e1-6130fbdc7fab",
+      "John Doe",
+      30,
+      true,
+      1,
+      ["reading", "video game"],
+      new Address("123-4567", "tokyo")
+    );
+    const maskedJson = person.toJSON(false, true);
+    const parsedJson = JSON.parse(maskedJson) as Person;
+    expect(parsedJson.name).toContain("*");
+    expect(parsedJson.address?.postalCode).toContain("*");
+    expect(parsedJson.address?.address).toContain("*");
+  });
+
+  test("fromJSON() should create a valid Person instance", () => {
+    const json = JSON.stringify({
+      id: "b46021c4-2cf2-4167-a31c-50c9d297e6b8",
+      name: "John Doe2",
+      age: 1,
+      isActive: false,
+      createAt: 1742483906966,
+      hobbies: ["reading", "video game"],
+      address: { postalCode: "123-4567", address: "tokyo" },
+    });
+
+    const person = Person.fromJSON(json) as Person;
+
+    expect(person).toBeInstanceOf(Person);
+    expect(person.id).toBe("b46021c4-2cf2-4167-a31c-50c9d297e6b8");
+    expect(person.name).toBe("John Doe2");
+    expect(person.age).toBe(1);
+    expect(person.isActive).toBeFalsy();
+    expect(person.createAt).toBe(1742483906966);
+    expect(person.hobbies).toStrictEqual(["reading", "video game"]);
+    expect(person.address?.postalCode).toBe("123-4567");
+    expect(person.address?.address).toBe("tokyo");
+  });
+
+  test("fromJSON() should not create a invalid Person instance", () => {
+    const json = JSON.stringify({
+      uudid: "b46021c4-2cf2-4167-a31c-50c9d297e6b8",
+      name: "John Doe2",
+      age: 1,
+      isActive: false,
+      createAt: 1742483906966,
+      hobbies: ["reading", "video game"],
+      address: { postalCode: "123-4567", address: "tokyo" },
+    });
+
+    expect(() => Person.fromJSON(json)).toThrow(/Invalid JSON: /);
+  });
+
+  test("clone() should create a deep copy", () => {
+    const person = new Person(
+      "4c581c64-94fc-4880-b6e1-6130fbdc7fab",
+      "John Doe",
+      30,
+      true,
+      1,
+      ["reading", "video game"],
+      new Address("123-4567", "tokyo")
+    );
+    const clone = person.clone();
+
+    expect(person.id).toBe(clone.id);
+    expect(person.name).toBe(clone.name);
+    expect(person.age).toBe(clone.age);
+    expect(person.isActive).toBe(clone.isActive);
+    expect(person.createAt).toBe(clone.createAt);
+    expect(person.hobbies).toStrictEqual(clone.hobbies);
+    expect(person.address?.postalCode).toBe(clone.address?.postalCode);
+    expect(person.address?.address).toBe(clone.address?.address);
+
+    expect(clone).not.toBe(person); // not equal reference
+  });
+
+  test("example() should create a example", () => {
+    const person = Person.example() as Person;
+
+    expect(person).toHaveProperty("id");
+    expect(person).toHaveProperty("name");
+    expect(person).toHaveProperty("isActive");
+    expect(person).toHaveProperty("age");
+    expect(person).toHaveProperty("createAt");
+    expect(person).toHaveProperty("hobbies");
+    expect(person).toHaveProperty("address");
+  });
+
+  test("getJsonSchema() should return valid JSON Schema", () => {
+    const schema = Person.getJsonSchema();
+
+    expect(schema).toHaveProperty("type", "object");
+    expect(schema.properties).toHaveProperty("id");
+    expect(schema.properties).toHaveProperty("name");
+    expect(schema.properties).toHaveProperty("isActive");
+    expect(schema.properties).toHaveProperty("age");
+    expect(schema.properties).toHaveProperty("createAt");
+    expect(schema.properties).toHaveProperty("hobbies");
+    expect(schema.properties).toHaveProperty("address");
+    expect(schema.required).toContain("name");
+    expect(schema.additionalProperties).toBe(false);
+  });
+
+  test("validate() should validate a valid Address instance", () => {
+    const person = Person.example() as Person;
+
+    expect(() => person.validate()).not.toThrow();
+  });
+
+  test("validate() should validate a invalid valid Address instance", () => {
+    const person = new Person();
+
+    expect(() => person.validate()).toThrow(/Invalid JSON: /);
+  });
+});
