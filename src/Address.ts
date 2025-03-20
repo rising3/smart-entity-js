9ab9b1dbec91d4b0e1ede1c1549d1ea585351b71
rising3 <michio.nakagawa@gmail.com>
2025-03-21 03:56:15 +0900

    feat: generic methods

---
+++ src/Address.ts
@@ -0,0 +1,21 @@
+import BaseEntity from "./BaseEntity";
+
+export default class Address extends BaseEntity<Address> {
+  protected _maskableFields = ["postalCode", "address"];
+  protected _requiredFields = ["postalCode", "address"];
+  protected _schemaHints = {
+    postalCode: { type: "string" },
+    address: { type: "string" },
+  };
+
+  constructor(
+    public postalCode: string | null = null,
+    public address: string | null = null
+  ) {
+    super();
+  }
+
+  static example(): Address {
+    return new Address("123-4567", "tokyo");
+  }
+}
---
+++ src/Person.ts
@@ -0,0 +1,44 @@
+import BaseEntity from "./BaseEntity";
+import Address from "./Address";
+
+export default class Person extends BaseEntity<Person> {
+  protected _maskableFields = ["name"];
+  protected _requiredFields = ["name"];
+  protected _schemaHints = {
+    id: { type: "string" },
+    name: { type: "string" },
+    age: { type: "number", nullable: true },
+    isActive: { type: "boolean", nullable: false },
+    createAt: { type: "number" },
+    hobbies: { type: "array", nullable: true },
+    address: {
+      type: "object",
+      schema: Address.getJsonSchema(),
+      nullable: true,
+    },
+  };
+
+  constructor(
+    public id: string = crypto.randomUUID(),
+    public name: string | null = null,
+    public age?: number,
+    public isActive: boolean = true,
+    public createAt: number = Date.now(),
+    public hobbies: string[] = [],
+    public address?: Address
+  ) {
+    super();
+  }
+
+  static example(): Person {
+    return new Person(
+      crypto.randomUUID(),
+      "John Doe",
+      Math.floor(Math.random() * 100) + 1,
+      true,
+      Date.now(),
+      ["reading", "video game"],
+      Address.example()
+    );
+  }
+}
