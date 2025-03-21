92fda8bf740ac6d7300186f1fe215fdf81c36cf0
rising3 <michio.nakagawa@gmail.com>
2025-03-21 18:58:42 +0900

    build: change script

---
+++ eslint.config.cjs
@@ -0,0 +1,50 @@
+const {
+    defineConfig,
+    globalIgnores,
+} = require("eslint/config");
+
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const globals = require("globals");
+const tsParser = require("@typescript-eslint/parser");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const {
+    includeIgnoreFile,
+} = require("@eslint/compat");
+
+const path = require("node:path");
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+module.exports = defineConfig([globalIgnores([
+    "**/.prettierrc",
+    "**/tsconfig.json",
+    "**/tsconfig.esm.json",
+]), includeIgnoreFile(gitignorePath), {
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.commonjs,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "script",
+    },
+
+    rules: {},
+}]);
